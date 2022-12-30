ccbdfd9e6dc119694ca53ac708ea390788f915da
Abdalla Mahmoud <117390537+Abdallah531@users.noreply.github.com>
2022-12-30 22:22:00 +0200

    Delete Software_Project directory

--- Software_Project/src/main/java/com/Advanced/Software_Project/Donation/Donations.java
+++
@@ -1,8 +0,0 @@
-package com.Advanced.Software_Project.Donation;
-
-import com.Advanced.Software_Project.Services.Services;
-import org.springframework.stereotype.Component;
-
-@Component
-public abstract class Donations extends Services {
-}
